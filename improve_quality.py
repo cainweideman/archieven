992ad7e23d260c6c6ac1d3ac9c0c45d55dcc97b3
CainWeideman <c.weideman@student.rug.nl>
2024-11-02 13:33:04 +0100

    Added documentation

--- improve_quality.py
+++ improve_quality.py
@@ -1,73 +1,202 @@
+"""
+Image Processing Script
+
+This script processes images stored within a specified directory structure. Each image is:
+- Converted to grayscale
+- Binarized (converted to black and white)
+- Cropped to remove a small margin from each edge
+- Saved in a subfolder within each directory.
+
+Modules:
+    - cv2: For image reading, processing, and saving.
+    - os: For directory and file handling.
+
+Functions:
+    - display_image: Display an image with matplotlib.
+    - grayscale: Convert an image to grayscale.
+    - binarize_image: Apply binary thresholding to a grayscale image.
+    - crop_image: Crop a specified margin from the image edges.
+    - process_images_in_directory: Main function that processes images in a directory structure.
+    - process_image: Function that processes one specific image
+    - process_directory: Function that processes the images in a specific directory
+"""
+
 import cv2
-from matplotlib import pyplot as plt
 import os
 
-
-def display(im_path):
-    dpi = 80
-    im_data = plt.imread(im_path)
-
-    height, width  = im_data.shape[:2]
-    
-    # What size does the figure need to be in inches to fit the image?
-    figsize = width / float(dpi), height / float(dpi)
-
-    # Create a figure of the right size with one axes that takes up the full figure
-    fig = plt.figure(figsize=figsize)
-    ax = fig.add_axes([0, 0, 1, 1])
-
-    # Hide spines, ticks, etc.
-    ax.axis('off')
-
-    # Display the image.
-    ax.imshow(im_data, cmap='gray')
-
-    plt.show()
-
-
 def grayscale(image):
+    """
+    Converts an image to grayscale.
+
+    Parameters:
+        image (numpy.ndarray): Input image in BGR format.
+
+    Returns:
+        numpy.ndarray: Grayscale image.
+    """
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 
-#image_file = 'cleaned/1968/1968_page_0008.jpg'
-#img = cv2.imread(image_file)
+def binarize_image(gray_image, threshold=160, max_value=230):
+    """
+    Applies binary thresholding to a grayscale image.
 
-#display(image_file)
+    Parameters:
+        gray_image (numpy.ndarray): Grayscale image.
+        threshold (int): Threshold value for binarization.
+        max_value (int): Maximum pixel value to use with the THRESH_BINARY thresholding.
 
-#gray_image = grayscale(img)
-#cv2.imwrite("temp/gray.jpg", gray_image)
+    Returns:
+        numpy.ndarray: Binarized (black-and-white) image.
+    """
+    _, binary_image = cv2.threshold(gray_image, threshold, max_value, cv2.THRESH_BINARY)
+    return binary_image
 
 
-#thresh, im_bw = cv2.threshold(gray_image, 200, 230, cv2.THRESH_BINARY)
-#cv2.imwrite("temp/bw_image.jpg", im_bw)
+def crop_image(image, crop_fraction=0.05):
+    """
+    Crops an image by removing a certain fraction from each edge.
+
+    Parameters:
+        image (numpy.ndarray): Input image to be cropped.
+        crop_fraction (float): Fraction of the image dimensions to crop from each side.
+
+    Returns:
+        numpy.ndarray: Cropped image.
+    """
+    height, width = image.shape[:2]
+    top, bottom = int(height * crop_fraction), int(height * (1 - crop_fraction))
+    left, right = int(width * crop_fraction), int(width * (1 - crop_fraction))
+    return image[top:bottom, left:right]
 
 
-#cropped = img[50:1880, 50:1200]
-#cv2.imwrite("temp/cropped.jpg", cropped)
-#display("temp/cropped.jpg")
+def process_images_in_directory(root_directory, threshold=160, crop_fraction=0):
+    """
+    Processes images in a specified directory structure:
+    - Converts each image to grayscale
+    - Applies binary thresholding
+    - Crops the image
+    - Saves the processed images in an 'images_improved' subfolder
 
-root_directory = 'data'
-directories = os.listdir(root_directory)
+    Parameters:
+        root_directory (str): Path to the root directory containing subdirectories with images.
+        threshold (int): Threshold value for binarization.
+        crop_fraction (float): Fraction of the image dimensions to crop from each side.
+    
+    Directory Structure:
+        root_directory/
+            ├── subdirectory1/
+            │   ├── images/
+			|	|	└──image1.jpg
+            │   └── images_improved/
+            │       └── improved_image1.jpg
+            ├── subdirectory2/
+            │   ├── images/
+			|	|	└──image2.jpg
+            │   └── images_improved/
+            │       └── improved_image2.jpg
+    """
+    for directory in os.listdir(root_directory):
+        path = os.path.join(root_directory, directory)
+        if not os.path.isdir(path):
+            continue
 
-for directory in directories:
-	path = os.path.join(root_directory, directory)
-	filenames = os.listdir(path)
-	file_path = os.path.join(path, filenames[-2])
-	
-	output_directory = os.path.join(path, 'images_improved')
-	if not os.path.exists(output_directory):
-		os.makedirs(output_directory)
+        filenames = os.listdir(path)
+        file_path = os.path.join(path, filenames[-2])
 
-	for image in os.listdir(file_path):
-		output_file = "improved_" + image
-		image_path = os.path.join(file_path, image)
-		img = cv2.imread(image_path)
-		gray_image = grayscale(img)
-		thresh, im_bw = cv2.threshold(gray_image, 160, 230, cv2.THRESH_BINARY)
-		height, width = img.shape[:2]
-		top = int(height * 0.05)
-		bottom = int(height * (1 - 0.05))
-		left = int(width * 0.05)
-		right = int(width * (1 - 0.05))
-		cropped_img = im_bw[top:bottom, left:right]
-		cv2.imwrite(os.path.join(output_directory, output_file), cropped_img)
+        output_directory = os.path.join(path, 'images_improved')
+        os.makedirs(output_directory, exist_ok=True)
+
+        for image_name in os.listdir(file_path):
+            image_path = os.path.join(file_path, image_name)
+            img = cv2.imread(image_path)
+
+            if img is None:
+                print(f"Warning: Unable to read {image_path}")
+                continue
+
+            gray_image = grayscale(img)
+            binary_image = binarize_image(gray_image, threshold)
+            cropped_img = crop_image(binary_image, crop_fraction)
+
+            output_file_path = os.path.join(output_directory, f"improved_{image_name}")
+            cv2.imwrite(output_file_path, cropped_img)
+            print(f"Processed and saved: {output_file_path}")
+
+
+def process_image(path_to_image, threshold=160, crop_fraction=0):
+	"""
+	Processes an image for a specific path:
+	- Converts image to grayscale
+	- Applies binary thresholding
+	- Crops the image
+	- Displays the processed image in a window
+
+	Parameters:
+		path_to_image (str): Path to the image.
+		threshold (int): Threshold value for binarization.
+		crop_fraction (float): Fraction of the image dimensions to crop from each side.
+	"""
+	image = cv2.imread(path_to_image)
+	if image is None:
+		print(f"Warning: Unable to read {path_to_image}")
+	else:
+		gray_image = grayscale(image)
+		binary_image = binarize_image(gray_image, threshold)
+		cropped_image = crop_image(binary_image, crop_fraction)
+
+		cv2.imshow("image", cropped_image)
+		cv2.waitKey(0)
+		cv2.destroyAllWindows()
+
+
+def process_directory(path_to_directory, threshold=160, crop_fraction=0):
+	"""
+	Processes every image in a specified directory:
+	- Converts image to grayscale
+	- Applies binary thresholding
+	- Crops the image
+	- Saves the processed images in an 'images_improved' subfolder
+
+	Parameters:
+		path_to_directory (str): Path to the directory.
+		threshold (int): Threshold value for binarization.
+        crop_fraction (float): Fraction of the image dimensions to crop from each side.
+        
+	Directory Structure:
+		directory/
+			├── images/
+			│   └── image1.jpg
+			├── images_improved/
+			│   └── improved_image1.jpg
+	"""
+	path_to_images = os.path.join(path_to_directory, "images")
+	output_directory = os.path.join(path_to_directory, 'images_improved')
+	os.makedirs(output_directory, exist_ok=True)
+
+	for image_name in os.listdir(path_to_images):
+		path_to_image = os.path.join(path_to_images, image_name)
+		image = cv2.imread(path_to_image)
+		gray_image = grayscale(image)
+		binary_image = binarize_image(gray_image, threshold)
+		cropped_image = crop_image(binary_image, crop_fraction)
+			
+		output_file_path = os.path.join(output_directory, f"improved_{image_name}")
+		cv2.imwrite(output_file_path, cropped_image)
+		print(f"Processed and saved: {output_file_path}")
+       
+
+threshold = 160
+crop_fraction = 0.05
+
+# Process one image
+path_to_image = "data/1854/images/1854_page_0008.jpg"
+process_image(path_to_image, threshold, crop_fraction)
+
+# Process images in one directory
+path_to_directory = "data/1854"
+#process_directory(path_to_directory)
+
+# Process images in all directories
+root_directory = "data"
+#process_images_in_directory(root_directory, threshold, crop_fraction)
