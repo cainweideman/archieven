4a698f4e3a2fa8c041139e45708bce6e18bd5729
WideMan <60692886+cainweideman@users.noreply.github.com>
2024-11-02 15:53:19 +0100

    Delete convert2pdf.py

--- convert2pdf.py
+++
@@ -1,9 +0,0 @@
-import os
-import img2pdf
-
-
-output_folder = 'cleaned/1968'
-image_files = [os.path.join(output_folder, f) for f in os.listdir(output_folder) if f.endswith(('.jpg', '.jpeg', '.png'))]
-
-with open('1964.pdf', 'wb') as f:
-	f.write(img2pdf.convert(image_files))
